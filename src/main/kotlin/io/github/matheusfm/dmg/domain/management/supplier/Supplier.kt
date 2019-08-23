3241b52afd91764fb9b2e83b69a8e08b02c03059
Matheus Moraes <matheusfaria.moraes@gmail.com>
2019-08-22 22:43:23 -0300

    searching with regex instead of text indexes because of partial words

--- src/main/kotlin/io/github/matheusfm/dmg/domain/management/supplier/Supplier.kt
+++ src/main/kotlin/io/github/matheusfm/dmg/domain/management/supplier/Supplier.kt
@@ -2,21 +2,20 @@ package io.github.matheusfm.dmg.domain.management.supplier
 
 import io.github.matheusfm.dmg.domain.management.DocumentType
 import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.index.TextIndexed
 import org.springframework.data.mongodb.core.mapping.Document
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
 
-@Document(collection = "suppliers", language = "portuguese")
+@Document(collection = "suppliers")
 data class Supplier(
     @field:Id val id: String?,
-    @field:TextIndexed @field:NotBlank val name: String?,
+    @field:NotBlank val name: String?,
     @field:Pattern(regexp = "(\\d{5}[-]?\\d{3})") val zipCode: String?,
     @field:NotBlank val address: String?,
     @field:NotBlank val city: String?,
     @field:NotBlank val state: String?,
-    @field:TextIndexed @field:NotBlank val document: String?,
+    @field:NotBlank val document: String?,
     @field:NotNull val documentType: DocumentType?,
     val stateRegistration: String?,
     val cnae: String?
